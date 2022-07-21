e07c8df7a20b6f4d2f5f06ce25e2b96338184ad2
Daniel Alexander <daniel.llumiquinga@hotmail.com>
2022-07-21 11:47:18 -0500

    [fix]webpack config

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import ProductItem from '../components/ProductItem';
-import useGetProducts from '../hooks/useGetProducts';
-import Loading from '../components/Loading';
-import '../styles/ProductList.scss';
+import React from "react";
+import ProductItem from "../components/ProductItem";
+import useGetProducts from "../hooks/useGetProducts";
+import Loading from "../components/Loading";
+import "../styles/ProductList.scss";
 
-const API = 'http://api.escuelajs.co/api/v1/products';
-
+const API = "https://api.escuelajs.co/api/v1/products";
 
 const ProductList = () => {
   const products = useGetProducts(API).products;
   const loading = useGetProducts(API).loading;
   return (
-    <section className='main-container'>
+    <section className="main-container">
       <div className="ProductList">
         {loading && <Loading />}
-        {products.map( product => (
+        {products.map((product) => (
           <ProductItem product={product} key={product.id} />
         ))}
-			</div>
+      </div>
     </section>
   );
 };
